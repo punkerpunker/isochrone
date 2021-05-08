8dbc73cce2b59806467d7f96d17785e3cc2876c0
punkerpunker <vazheningleb@yandex.ru>
2021-05-08 17:05:40 +0300

    Refreshed function

--- containers/pgrouting/init.sql
+++ containers/pgrouting/init.sql
@@ -2,17 +2,14 @@ CREATE EXTENSION postgis;
 CREATE EXTENSION pgrouting;
 
 CREATE OR REPLACE FUNCTION public.catchment_area(
-    x double precision,
-    y double precision,
-    cost double precision,
-    type text)
+	x double precision,
+	y double precision,
+	cost double precision)
     RETURNS geometry
     LANGUAGE 'plpgsql'
-
     COST 100
-    VOLATILE 
+    VOLATILE PARALLEL UNSAFE
 AS $BODY$
-
 DECLARE
   speed text;
   reverse_cost_time_column text;
@@ -32,16 +29,7 @@ tab_network = 'ways';
 tab_grid = 'grid';
 tab_res_lines = concat('lines_', now()::text);
 node_id = -1;
-IF type = 'car' then
-    reverse_cost_time_column = 'rcost_time_car';
-    cost_time_column = 'cost_time_car';
-    -- speed = '(maxspeed_forward/10)';
-    speed = '20';
-ELSE
-    reverse_cost_time_column = 'cost_time_ped';
-    cost_time_column = 'cost_time_ped';
-    speed = '5';
-end if;
+speed = '5';
 
 EXECUTE 'SELECT id
         FROM '||tab_network||'_vertices_pgr
@@ -51,24 +39,22 @@ EXECUTE 'SELECT id
 if node_id > -1 then
         statement = 'CREATE TEMP TABLE "'||tab_res_lines||'" AS (SELECT gid as id, the_geom as geom, node from '||tab_network||'
       join (SELECT node,edge FROM pgr_drivingDistance(
-        ''SELECT gid as id, source, target, cost, reverse_cost FROM (SELECT gid, source, target, '||cost_time_column||' as cost, '||reverse_cost_time_column||' as reverse_cost, the_geom
+        ''SELECT gid as id, source, target, cost, reverse_cost FROM (SELECT gid, source, target, cost, reverse_cost, the_geom
       FROM '||tab_network||'
       WHERE ST_contains(ST_Transform(ST_Buffer(ST_Transform(ST_SetSRID(ST_MakePoint('||x||','||y||'),4326), 3395),
         '||cost||'*'||speed||'*1000.0/3600.0*1.3, 16), 4326), the_geom)) as preSelection'',
         '||node_id||', '||cost||', false
       )) d on '||tab_network||'.gid=d.edge);';
       EXECUTE statement;
-      -- workaround для новой версии pgrouting который не хочет работать с CGAL
-      --statement = 'select st_concavehull(st_setsrid((SELECT ST_Collect(geom) FROM "'||tab_res_lines||'"), 4326), 0.7);';
-      statement = 'select geom from ST_DumpRings(st_setsrid(pgr_pointsAsPolygon(''SELECT '||tab_network||'_vertices_pgr.id::integer, ST_X(the_geom)::float AS x, ST_Y(the_geom)::float AS y
-        FROM '||tab_network||'_vertices_pgr join "'||tab_res_lines||'" on '||tab_network||'_vertices_pgr.id = "'||tab_res_lines||'".node''),4326));';
+      statement = 'SELECT ST_ConcaveHull(ST_Collect(the_geom), 0.7)
+         		   FROM '||tab_network||'_vertices_pgr v join "'||tab_res_lines||'" l on v.id = l.node;';
       BEGIN
             EXECUTE statement into res_area;
             -- Иногда не можем построить зону, если так - строим круг
-            exception when others then
-            radius = speed::float * cost*10/36;
-            statement = 'select ST_Buffer(ST_SetSRID(ST_MakePoint('||x||','||y||'),4326)::geography, '|| radius::text ||')';
-            EXECUTE statement into res_area;
+--             exception when others then
+--             radius = speed::float * cost*10/36;
+--             statement = 'select ST_Buffer(ST_SetSRID(ST_MakePoint('||x||','||y||'),4326)::geography, '|| radius::text ||')';
+--             EXECUTE statement into res_area;
                 
       END;
       -- Иногда возвращает пустую геометрию, если так - строим круг
@@ -85,5 +71,5 @@ end if;
 END
 $BODY$;
 
-ALTER FUNCTION public.catchment_area(double precision, double precision, double precision, text)
+ALTER FUNCTION public.catchment_area(double precision, double precision, double precision)
     OWNER TO postgres;
