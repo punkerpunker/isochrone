dc91fde1a27c809701a14e273ea0e7cc2345e1c7
punkerpunker <vazheningleb@yandex.ru>
2021-05-09 16:51:53 +0300

    Added swagger API microservice

--- containers/app/app.py
+++ containers/app/app.py
@@ -1,5 +1,7 @@
-import subprocess
 import os
+import subprocess
+import connexion
+import sqlalchemy
 from shapely.geometry import Polygon
 from shapely import wkt
 
@@ -8,6 +10,7 @@ DB_PASSWORD = os.environ['POSTGRES_PASSWORD']
 RAW_DIR = '/osm/pbf'
 CONVERTED_DIR = '/osm/pbf/converted'
 MAPCONFIG_PATH = '/osm/mapconfig.xml'
+engine = sqlalchemy.create_engine(f'postgresql+psycopg2://postgres:{DB_PASSWORD}@pgrouting/postgres')
 
 
 class Loader:
@@ -33,14 +36,21 @@ class Loader:
                                    f'--username postgres --password {password}'], shell=True)
 
 
-def get_catchment_area(cur, long, lat, sec):
-    query = "select ST_AsText(catchment_area(%s, %s, %s,))" % (long, lat, sec)
-    cur.execute(query)
-    return Polygon(wkt.loads(cur.fetchone()[0]))
+def isochrone(X, Y, sec):
+    query = "select ST_AsText(catchment_area(%s, %s, %s)) as polygon" % (X, Y, sec)
+
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        for row in result:
+            return row['polygon']
 
 
 if __name__ == "__main__":
     Loader.create_dirs(CONVERTED_DIR)
     Loader.convert(RAW_DIR, CONVERTED_DIR)
     Loader.load(CONVERTED_DIR, MAPCONFIG_PATH, DB_PASSWORD)
+
+    app = connexion.App(__name__, specification_dir='./')
+    app.add_api('swagger.yaml')
+    app.run(port=1769)
 
